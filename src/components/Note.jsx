5a5a47251cc92c4e5b3139ac019ea69615fec501
OllieRott <103285545+OllieRott@users.noreply.github.com>
2023-02-04 20:16:06 +0100

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Note = function () {
+	return (
+		<div className="note">
+			<h1>This is the paragraph</h1>
+			<p>This is the content</p>
+		</div>
+	);
+};
